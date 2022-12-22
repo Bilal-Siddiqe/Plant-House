49098c9187ca71afb8a0cb0825213a8c339daa13
Bilal-Siddiqe <dev.bilalsiddique@gmail.com>
2022-12-22 17:01:04 +0500

    run build added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
 
                 <a class="navbar-brand text-success fw-bolder " href="#" >Plant House</a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
